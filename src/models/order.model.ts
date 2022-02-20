d74e3c62933cf15ef7191998b529c8c9671054a8
Andrea <andrea.munozr@autonoma.edu.co>
2022-02-20 16:43:32 -0500

    Mi primer crud en Loopback

---
+++ src/models/order.model.ts
@@ -0,0 +1,52 @@
+import {Entity, model, property, belongsTo, hasOne} from '@loopback/repository';
+import {Person} from './person.model';
+import {Product} from './product.model';
+
+@model({settings: {strict: false}})
+export class Order extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  id?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  producto: string;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  cantidad: number;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  state: number;
+
+  @belongsTo(() => Person)
+  personId: string;
+
+  @hasOne(() => Product)
+  product: Product;
+  // Define well-known properties here
+
+  // Indexer property to allow additional data
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  [prop: string]: any;
+
+  constructor(data?: Partial<Order>) {
+    super(data);
+  }
+}
+
+export interface OrderRelations {
+  // describe navigational properties here
+}
+
+export type OrderWithRelations = Order & OrderRelations;
---
+++ src/models/person.model.ts
@@ -0,0 +1,54 @@
+import {Entity, model, property, hasMany} from '@loopback/repository';
+import {Order} from './order.model';
+
+@model({settings: {strict: false}})
+export class Person extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  id?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  name: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  email: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  contact: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  password: string;
+
+  @hasMany(() => Order)
+  orders: Order[];
+  // Define well-known properties here
+
+  // Indexer property to allow additional data
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  [prop: string]: any;
+
+  constructor(data?: Partial<Person>) {
+    super(data);
+  }
+}
+
+export interface PersonRelations {
+  // describe navigational properties here
+}
+
+export type PersonWithRelations = Person & PersonRelations;
